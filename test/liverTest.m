bc7e7c560785dc44716820750ed4bd0699f25c94
mtefagh <mtefagh@stanford.edu>
2019-12-22 15:31:44 +0330

    added liver tests

--- test/liverTest.m
+++ test/liverTest.m
@@ -3,13 +3,17 @@ clear all; close all; clc;
 load('consistRecon1.mat');
 n = length(model.rxns);
 solver = 'gurobi';
+changeCobraSolver(solver);
 % extracting the set of core reactions
 core = load('C_liver.mat').C;
-LP = zeros(2, 100);
-runtime = zeros(3, 100);
-performance = zeros(4, 100);
-errors = true(3, 100);
-for k = 1:100
+% setting the number of iterations
+iterations = 10;
+LP = zeros(2, iterations);
+runtime = zeros(3, iterations);
+performance = zeros(4, iterations);
+errors = true(3, iterations);
+finalScore = zeros(3, iterations);
+for k = 1:iterations
     disp(k);
     % FASTCORE
     time1 = tic;
@@ -25,12 +29,18 @@ for k = 1:100
         tempmodel.lb = model.lb(coreRxnBool);
         tempmodel.ub = model.ub(coreRxnBool);
         tempmodel.rxns = model.rxns(coreRxnBool);
+        tempmodel.rxnNames = model.rxnNames(coreRxnBool);
+        tempmodel.rules = model.rules(coreRxnBool);
         tempmodel.c = model.c(coreRxnBool);
         tempmodel.mets = model.mets;
+        tempmodel.b = model.b;
         A = fastcc(tempmodel, 1e-4);
         if all(A.' == 1:length(A))
             errors(1, k) = false;
         end
+        taskReport = checkMetabolicTasks(tempmodel, 'liverTests.xlsx');
+        taskReport = split(taskReport(211, 3), '/');
+        finalScore(1, k) = str2double(taskReport(1));
     end
     % SWIFTCORE w/o reduction
     time2 = tic;
@@ -45,12 +55,18 @@ for k = 1:100
         tempmodel.lb = model.lb(coreInd);
         tempmodel.ub = model.ub(coreInd);
         tempmodel.rxns = model.rxns(coreInd);
+        tempmodel.rxnNames = model.rxnNames(coreInd);
+        tempmodel.rules = model.rules(coreInd);
         tempmodel.c = model.c(coreInd);
         tempmodel.mets = model.mets;
+        tempmodel.b = model.b;
         A = fastcc(tempmodel, 1e-4);
         if all(A.' == 1:length(A))
             errors(2, k) = false;
         end
+        taskReport = checkMetabolicTasks(tempmodel, 'liverTests.xlsx');
+        taskReport = split(taskReport(211, 3), '/');
+        finalScore(2, k) = str2double(taskReport(1));
     end
     % SWIFTCORE w/ reduction
     time3 = tic;
@@ -65,12 +81,18 @@ for k = 1:100
         tempmodel.lb = model.lb(coreIndReduce);
         tempmodel.ub = model.ub(coreIndReduce);
         tempmodel.rxns = model.rxns(coreIndReduce);
+        tempmodel.rxnNames = model.rxnNames(coreIndReduce);
+        tempmodel.rules = model.rules(coreIndReduce);
         tempmodel.c = model.c(coreIndReduce);
         tempmodel.mets = model.mets;
+        tempmodel.b = model.b;
         A = fastcc(tempmodel, 1e-4);
         if all(A.' == 1:length(A))
             errors(3, k) = false;
         end
+        taskReport = checkMetabolicTasks(tempmodel, 'liverTests.xlsx');
+        taskReport = split(taskReport(211, 3), '/');
+        finalScore(3, k) = str2double(taskReport(1));
     end
     % calculating the size of the intersection of the answers of the three algorithms above
     performance(4, k) = sum(coreIndReduce + coreInd + coreRxnBool == 3);
@@ -81,21 +103,28 @@ fprintf('SWIFTCORE w/o reduction returns wrong answers in %d%% of cases!\n', rou
 fprintf('SWIFTCORE w/ reduction returns wrong answers in %d%% of cases!\n', round(100*sum(errors(3,:))/size(errors,2)));
 % drawing scatterplots for the comparison of the number of LPs
 figure();
-plot(1:100,LP(1,:),'o',1:100,LP(2,:),'*');
+plot(1:iterations,LP(1,:),'o',1:iterations,LP(2,:),'*');
 legend({'SWIFTCORE w/o reduction','SWIFTCORE w/ reduction'});
 xlabel('iteration');
 ylabel('number of solved LPs');
 savefig('LPLiver');
 % drawing scatterplots for the comparison of the runtime
 figure();
-plot(1:100,runtime(1,:),'o',1:100,runtime(2,:),'*',1:100,runtime(3,:),'+');
+plot(1:iterations,runtime(1,:),'o',1:iterations,runtime(2,:),'*',1:iterations,runtime(3,:),'+');
 legend({'FASTCORE','SWIFTCORE w/o reduction','SWIFTCORE w/ reduction'},'Location','northeast');
 xlabel('iteration');
 ylabel('runtime in seconds');
 savefig('runtimeLiver');
+% drawing scatterplots for the comparison of the number of passed tests
+figure();
+plot(1:iterations,finalScore(1,:),'o',1:iterations,finalScore(2,:),'*',1:iterations,finalScore(3,:),'+');
+legend({'FASTCORE','SWIFTCORE w/o reduction','SWIFTCORE w/ reduction'},'Location','northeast');
+xlabel('iteration');
+ylabel('number of passed tests');
+savefig('testsLiver');
 % drawing scatterplots for the comparison of the sparsity
 figure();
-plot(1:100,performance(1,:),'o',1:100,performance(2,:),'*',1:100,performance(3,:),'+',1:100,performance(4,:),'x');
+plot(1:iterations,performance(1,:),'o',1:iterations,performance(2,:),'*',1:iterations,performance(3,:),'+',1:iterations,performance(4,:),'x');
 legend({'FASTCORE','SWIFTCORE w/o reduction','SWIFTCORE w/ reduction','intersection'},'Location','southeast');
 xlabel('iteration');
 ylabel('size of the subnetwork');
